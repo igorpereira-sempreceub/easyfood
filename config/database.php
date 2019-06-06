2aff0e600172910dc744bbac8115e554ce0f8fcf
Igor Pereira <igor.pereira@exata.it>
2019-06-06 16:17:40 -0300

    usando o mogodb atlas

--- config/database.php
+++ config/database.php
@@ -93,14 +93,16 @@ return [
 
         'mongodb' => [
             'driver'   => 'mongodb',
-            'host'     => env('MONGO_DB_HOST', 'localhost'),
-            'port'     => env('MONGO_DB_PORT', 27017),
-            'database' => env('MONGO_DB_DATABASE'),
-            'username' => env('MONGO_DB_USERNAME'),
-            'password' => env('MONGO_DB_PASSWORD'),
-            'options'  => [
-                'database' => 'afrodite'
-            ]
+            // 'host'     => 'cluster0-lvvzc.azure.mongodb.net',
+            // 'port'     => env('MONGO_DB_PORT', 27017),
+            // 'database' => env('MONGO_DB_DATABASE'),
+            // 'username' => env('MONGO_DB_USERNAME'),
+            // 'password' => env('MONGO_DB_PASSWORD'),
+            'dsn' => 'mongodb+srv://vuejs_express:vqFxNT9Hvu4BmVK@cluster0-lvvzc.azure.mongodb.net/test?retryWrites=true&w=majority',
+            'database' => 'afrodite',
+            // 'options'  => [
+            //     'database' => 'afrodite'
+            // ]
         ],
 
     ],
